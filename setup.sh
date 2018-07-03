2825a8fde139569b39740003f723dd3dbf90248a
Rimenes Ribeiro <rima@rimenesribeiro.com>
2018-07-02 22:44:07 -0300

    Kickstart a setup script.

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Install Xcode Command Line Tools.
+xcode-select --install
+
+# Install Homebrew.
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
